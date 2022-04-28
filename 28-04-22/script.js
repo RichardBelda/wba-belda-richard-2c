f9f3153f0340e927d45bc477bef0532582f07119
RichardBelda <risa.belda@gmail.com>
2022-04-28 11:36:51 +0200

    Basic script file added

---
+++ 28-04-22/script.js
@@ -0,0 +1,13 @@
+alert("Buchty jsou dobré");
+
+var number = 9;
+
+var text = "Hello World JS";
+
+var ready = true;
+
+var notready = false;
+
+function buttonClick() {
+    console.log("Button: Press the button clicked successfully");
+}

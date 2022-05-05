65256a6b27452245390f0778df5c4577de53543b
RichardBelda <risa.belda@gmail.com>
2022-05-05 11:40:25 +0200

    End of lesson

--- 28-04-22/script.js
+++ 28-04-22/script.js
@@ -1,13 +1,25 @@
-alert("Buchty jsou dobré");
+// alert("Buchty jsou dobré");
 
-var number = 9;
+// var number = 9;
 
-var text = "Hello World JS";
+// var text = "Hello World JS";
 
-var ready = true;
+// var ready = true;
 
-var notready = false;
+// var notready = false;
 
-function buttonClick() {
-    console.log("Button: Press the button clicked successfully");
+// function buttonClick() {
+//     console.log("Button: Press the button clicked successfully");
+// }
+
+let button1 = document.getElementById("button");
+console.log(button1);
+
+button1.addEventListener("click", () => {
+    console.log("Mám rád koláče :)");
+})
+
+function toggleClass() {
+    let div = document.getElementById("toggle");
+    div.classList.toggle("divclass");
 }
